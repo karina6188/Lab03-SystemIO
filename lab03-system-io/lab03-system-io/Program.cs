cecdd0331e4518b4ea4842713860b04d73100a20
Karina Chen <karina6188@gmail.com>
2019-10-19 15:39:57 -0700

    AddWords only adds valid input

--- lab03-system-io/lab03-system-io/Program.cs
+++ lab03-system-io/lab03-system-io/Program.cs
@@ -27,10 +27,10 @@ namespace lab03_system_io
             Console.Clear();
             Console.WriteLine("Welcome to the Word Guess Game!");
             Console.WriteLine("Choose from the following menu to start.");
-            Console.WriteLine("1) View Words");
-            Console.WriteLine("2) Remove Words");
+            Console.WriteLine("1) Start a Game");
+            Console.WriteLine("2) View Words");
             Console.WriteLine("3) Add Words");
-            Console.WriteLine("4) Start a Game");
+            Console.WriteLine("4) Remove Words");
             Console.WriteLine("5) Exit");
             string selection = Console.ReadLine();
             string path = "../../../../guessWords.txt";
@@ -39,24 +39,26 @@ namespace lab03_system_io
                 switch (selection)
                 {
                     case "1":
+                        Console.WriteLine("Let's start the game!");
+                        Console.WriteLine("Enter a character to guess the word.");
+                        StartGame();
+                        return true;
+
+                    case "2":
                         ViewWords(path);
                         Console.ReadLine();
                         return true;
 
-                    case "2":
-                        RemoveWords();
-                        return true;
-
                     case "3":
                         Console.WriteLine("Enter a word that you want to add to the game.");
-                        string[] newWord = { Console.ReadLine()};
+                        string[] newWord = { Console.ReadLine() };
                         AddWords(path, newWord);
                         return true;
 
                     case "4":
-                        Console.WriteLine("Let's start the game!");
-                        Console.WriteLine("Enter a character to guess the word.");
-                        StartGame();
+                        Console.WriteLine("Enter a word that you want to remove from the game.");
+                        string removeWord = Console.ReadLine();
+                        RemoveWords(path, removeWord);
                         return true;
 
                     case "5":
@@ -89,15 +91,32 @@ namespace lab03_system_io
             }
         }
 
-        public static void RemoveWords()
+        public static void RemoveWords(string path, string word)
         {
-            Console.WriteLine("Enter a word that you want to remove from the game.");
-            string removeWord = Console.ReadLine();
+            string allWords = File.ReadAllText(path);
+            string[] newWordsArray = new string[allWords.Length - 1];
+            for (int i = 0; i < allWords.Length; i++)
+            {
+                if (allWords.Contains(word))
+                {
+                    Console.WriteLine("THIS WORKS");
+                    Console.ReadLine();
+                }
+            }
+
+  
+            for (int i = 0; i < newWordsArray.Length; i++)
+            {
+
+            }
         }
 
         public static void AddWords(string path, string[] word)
         {
-            File.AppendAllLines(path, word);
+            if (word[0].Length > 0)
+            {
+                File.AppendAllLines(path, word);
+            }
         }
 
         public static void StartGame()
