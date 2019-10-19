e919ef008b1310914c797f9f1031965d07ea71e8
Karina Chen <karina6188@gmail.com>
2019-10-19 01:32:58 -0700

    set up repo and added UserInterface method

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Lab03-SystemIO
+
+## About This Program
+Word Guess Game
+
+Author: Karina Chen
+
+## Description
+This is a C# console application that allows the user to play a word guess game. The user will see a main menu once the program is opened. The user can choose to 1) view all the guess words, 2) add a new word, 3) remove a word, 4) start a game, or 5) exit the game. The word guess game shows a hidden word and asks the user to guess the characters of the word. The user only knows the length of the word. If the user input matches any of the characters of the word,the character becomes visible. The user needs to keep guessing the correct characters of the word until all the characters are visible. Then the user wins the game.
+
+## Getting Started
+Clone this repository to your local machine.
+
+$ git clone [https://github.com/karina6188/Lab03-SystemIO.git]
+
+To run the program from Visual Studio:
+Select File -> Open -> Project/Solution
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the Lab03-SystemIO directory.
+
+Then select and open lab03-system-io.
+
+## To Run This Program
+Open the program using Visual Studio and click "Start" button to run this program.
+
+
+## Visuals
+
+Application Start
+
+![Alt execution capture](/)
+
+
+Using the Application
+
+![Alt execution capture](/)
+
+Using the Application
+
+![Alt execution capture](/)
+
+Using the Application
+
+![Alt execution capture](/)
+
+Application End
+
+![Alt execution capture](/)
+
+## Change Log
+
+1.1: Set up the program and added UserInterface method. - 2019 Oct 18
+
+1.2: 
---
+++ lab03-system-io/lab03-system-io/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+namespace lab03_system_io
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool game = true;
+                while (game)
+                {
+                    game = UserInterface();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+
+        public static bool UserInterface()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Word Guess Game!");
+            Console.WriteLine("Choose from the following menu to start.");
+            Console.WriteLine("1) View Words");
+            Console.WriteLine("2) Remove Words");
+            Console.WriteLine("3) Add Words");
+            Console.WriteLine("4) Start a Game");
+            Console.WriteLine("5) Exit");
+            string selection = Console.ReadLine();
+            string path = "../../guessWords.txt";
+            try
+            {
+                switch (selection)
+                {
+                    case "1":
+                        ViewWords(path);
+                        return true;
+
+                    case "2":
+                        RemoveWords();
+                        return true;
+
+                    case "3":
+                        AddWords();
+                        return true;
+
+                    case "4":
+                        Console.WriteLine("Let's start the game!");
+                        Console.WriteLine("Enter a character to guess the word.");
+                        StartGame();
+                        return true;
+
+                    case "5":
+                        Console.WriteLine("Bye bye! See you next time.");
+                        Console.ReadLine();
+                        return false;
+
+                    default:
+                        Console.WriteLine("Your selection is invalid.");
+                        Console.WriteLine("Please select from the available options.");
+                        Console.ReadLine();
+                        return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("That is not a valid entry.");
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                return true;
+            }
+        }
+
+        public static void ViewWords(string path)
+        {
+            string allWords = File.ReadAllText(path);
+            Console.WriteLine(allWords);
+        }
+
+        public static void RemoveWords()
+        {
+            Console.WriteLine("Enter a word that you want to remove from the game.");
+            string removeWord = Console.ReadLine();
+        }
+
+        public static void AddWords()
+        {
+            Console.WriteLine("Enter a word that you want to add to the game.");
+            string addWord = Console.ReadLine();
+        }
+
+        public static void StartGame()
+        {
+            char guessCharacter = Console.ReadLine()[0];
+        }
+    }
+}
